7cd2cff0e724480db84ca889c7be9d86dde55156
AikawaKai <marco.odore@studenti.unimi.it>
2017-12-02 14:17:19 +0100

    parsed

--- dataset_parser.r
+++ dataset_parser.r
@@ -1,9 +1,10 @@
 wd <- getwd()
 dataset_path <- paste(wd,"/adult.csv",sep="")
 csv_data <- read.csv(file=dataset_path,sep=",",header = TRUE, stringsAsFactors = TRUE)
-(csv_data[3,3])
+new_header <- colnames(csv_data[2,])
+head(csv_data)
 for (i in 1:length(csv_data)){
   csv_data[,i] = as.numeric(csv_data[,i])
-  print(i)
 }
-print(csv_data[,3][48842])
+print(csv_data[,15])
+write.csv(csv_data, file="./parsed.csv", sep = ',',row.names = FALSE)
