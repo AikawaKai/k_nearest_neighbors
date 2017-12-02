6dfbfd123949761dc4af085ce807d675d8117361
AikawaKai <marco.odore@studenti.unimi.it>
2017-12-02 15:25:51 +0100

    updated

--- knn.r
+++ knn.r
@@ -1,7 +1,13 @@
 library(caret)
 path <- getwd()
 csv_readed = read.csv(file=paste(path,"/parsed.csv",sep = ""))
-head(csv_readed)
-train_control <- trainControl(method="cv", number=10)
-str(csv_readed)
-model <- train(data=csv_readed,income~.,  method = "knn", trControl = train_control, metric = "Accuracy")
+csv_readed$income = factor(csv_readed$income)
+
+crossValidationByCaret <- function(x)  {
+  train_control <- trainControl(method="cv", number=10)
+  model <- train(data=csv_readed,income~.,  method = "knn", trControl = train_control)
+  return(model)
+}
+
+crossValidationByCaret()
+
