0ad0cbbaa03c7669df1f9f8ebf18b1d4bbed18c2
AikawaKai <marco.odore@studenti.unimi.it>
2017-12-19 14:30:20 +0100

    updated

--- test_sequential.r
+++ test_sequential.r
@@ -2,7 +2,7 @@ source("/home/kai/Documents/Unimi/MetodiStatisticiApp/k_nearest_neighbors/knn_li
 
 file <- paste(path,"/parsed.csv",sep="")
 csv_readed <- read.csv(file=file)
-csv_readed <- csv_readed[1:5000,]
+csv_readed <- csv_readed[1:400,]
 myK <- c(1, 3, 5, 7, 9, 11, 13, 15, 17, 19, 21, 23, 25, 27, 29, 31, 33, 35, 37, 39, 41, 43, 45)
 set.seed(1)
 S <- matrix(nrow=0, ncol=15)
