aa31b2cb360c01a337e0339cb11407d1e4964b99
AikawaKai <marco.odore@studenti.unimi.it>
2018-01-05 19:06:18 +0100

    updated

--- pca_analysis.r
+++ pca_analysis.r
@@ -1,8 +1,8 @@
 source("/home/kai/Documents/Unimi/MetodiStatisticiApp/k_nearest_neighbors/knn_library.r")
 
-file <- paste(path,"/parsed.csv",sep="")
+file <- paste(path,"/parsed_normalized.csv",sep="")
 csv_readed <- read.csv(file=file)
-features <- csv_readed[,1:14]
+features <- csv_readed[,1:88]
 pca <- prcomp(features, center = TRUE, scale. = TRUE)
-plot(pca$x[,13:14], col=csv_readed[,15])
+plot(pca$x[,1:2], col=csv_readed[,89])
 ncol(pca$x)
