0cac1ec46032c78f83cb7b856c9a6fe05044ea02
AikawaKai <marco.odore@studenti.unimi.it>
2018-01-06 01:03:53 +0100

    updated

--- knnByCaret.r
+++ knnByCaret.r
@@ -2,17 +2,11 @@ library(caret)
 path <- "/home/kai/Documents/Unimi/MetodiStatisticiApp/k_nearest_neighbors"
 crossValidationByCaret <- function(data_set, class, fold)  {
   train_control <- trainControl(method="cv", number=fold, returnResamp = "all")
-  model <- train(data=data_set,income~.,  method = "knn", trControl = train_control)
+  model <- train(data=data_set,income~.,  method = "knn", trControl = train_control, tuneLength = 26)
   return(model)
 }
 
-classifyKnn <- function(model, input, k) {
-  for(row in model[1:14]){
-    print(row);
-  }
-}
-
-file_ <- paste(path,"/parsed.csv",sep="")
+file_ <- paste(path,"/parsed_normalized.csv",sep="")
 csv_readed <- read.csv(file=file_)
 csv_readed$income <- factor(csv_readed$income)
 model <- crossValidationByCaret(csv_readed, income, 5)
--- pca_analysis.r
+++ pca_analysis.r
@@ -4,5 +4,5 @@ file <- paste(path,"/parsed_normalized.csv",sep="")
 csv_readed <- read.csv(file=file)
 features <- csv_readed[,1:88]
 pca <- prcomp(features, center = TRUE, scale. = TRUE)
-plot(pca$x[,1:2], col=csv_readed[,89])
+plot(pca$x[,2:3], col=csv_readed[,89])
 ncol(pca$x)
